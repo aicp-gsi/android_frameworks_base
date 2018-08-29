767bcee1120757df08bcd25ff7a94d4c243699ed
Pierre-Hugues Husson <phh@phh.me>
2018-08-29 11:05:54 +0200

    Add a property to override pre-o max aspect ratio
    Change-Id: I082badf0fff51838ce531a83e06fef1a1fe4f32c

--- services/core/java/com/android/server/am/ActivityRecord.java
+++ services/core/java/com/android/server/am/ActivityRecord.java
@@ -162,6 +162,7 @@ import android.os.PersistableBundle;
 import android.os.Process;
 import android.os.RemoteException;
 import android.os.SystemClock;
+import android.os.SystemProperties;
 import android.os.UserHandle;
 import android.os.storage.StorageManager;
 import android.service.voice.IVoiceInteractionSession;
@@ -2363,18 +2364,7 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
     // TODO(b/36505427): Consider moving this method and similar ones to ConfigurationContainer.
     private void computeBounds(Rect outBounds) {
         outBounds.setEmpty();
-
         float maxAspectRatio = info.maxAspectRatio;
-        if (maxAspectRatio != 0.0f && higherAspectRatio && service.getAspectRatioApps() != null) {
-            if (service.getAspectRatioApps().contains(packageName)) {
-                if (ActivityManagerService.DEBUG_ASPECT_RATIO) {
-                    Log.d(ActivityManagerService.TAG_DEBUG_ASPECT_RATIO,
-                            "Force aspect ratio for " + packageName + " " + maxAspectRatio);
-                }
-                maxAspectRatio = mFullScreenAspectRatio;
-            }
-        }
-
         final ActivityStack stack = getStack();
         if (task == null || stack == null || task.inMultiWindowMode() || maxAspectRatio == 0
                 || isInVrUiMode(getConfiguration())) {
@@ -2385,6 +2375,12 @@ final class ActivityRecord extends ConfigurationContainer implements AppWindowCo
             return;
         }
 
+        if(info.applicationInfo.targetSdkVersion < O) {
+            try {
+                maxAspectRatio = Float.parseFloat(SystemProperties.get("persist.sys.max_aspect_ratio.pre_o", ""));
+            } catch (Throwable t) {}
+            Log.d("PHH", "Overrode aspect ratio because pre-o to " + maxAspectRatio);
+        }
         // We must base this on the parent configuration, because we set our override
         // configuration's appBounds based on the result of this method. If we used our own
         // configuration, it would be influenced by past invocations.
